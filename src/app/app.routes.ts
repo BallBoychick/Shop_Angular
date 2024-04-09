1545f5b8af42e951caf062e2f420636ec439dd64
Mikhail <smirnow.mishas2017@gmail.com>
2024-04-09 09:28:24 +0600

    fc

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
+import { CartPageComponent } from './cart-page/cart-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: MainLayoutComponent},
+    {path: 'cart', component: CartPageComponent} 
+];
---
+++ src/app/cart-page/cart-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-cart-page',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './cart-page.component.html',
+  styleUrl: './cart-page.component.scss'
+})
+export class CartPageComponent {
+
+}
